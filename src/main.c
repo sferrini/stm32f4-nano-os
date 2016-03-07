21e573d0f381b85729e8c5a8ea4ec2d7f01e284a
sferrini <sferrini@hotmail.it>
2016-03-07 23:33:52 +0100

    Created some threads from main

--- src/main.c
+++ src/main.c
@@ -1,6 +1,29 @@
 
+#include "threads.h"
+
+void first_thread(void *data)
+{
+
+}
+
+void second_thread(void *data)
+{
+
+}
+
+void third_thread(void *data)
+{
+
+}
+
 int main(void)
 {
+
+  thread_create(first_thread, NULL);
+  thread_create(second_thread, NULL);
+  thread_create(third_thread, NULL);
+
+  threads_run();
 
   return 0;
 }
